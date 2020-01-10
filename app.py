7a49a5022da9456c8a86da7bd24b575b96bf0dc6
Deoh <darylhyde_13@hotmail.com>
2020-01-10 23:58:06 +0000

    Flask/MongoDB connected & drink.html added

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+from os import path
+if path.exists("env.py"):
+    import env 
+
 
 app = Flask(__name__)
 
+#app.config["MONGO_DBNAME"] = 'recipe_book'
+#app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://new_root_dh:r00tUserDh@myfirstcluster-lhfu5.mongodb.net/recipe_book?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = "recipe_book"
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return "hello world!"
+@app.route('/get_drink')
+def get_drink():
+    return render_template("drink.html", drink=mongo.db.drink.find())
 
 
 if __name__ == '__main__':
